ec3d4fe52096157d55f09b253780dd78278355ae
Jean Kuo <jeankuo5011@gmail.com>
2025-05-01 15:10:28 +0800

    算式跟求和函式粗暴融合

--- Q1_4/1_4module.js
+++ Q1_4/1_4module.js
@@ -43,3 +43,40 @@ export function calculationFormula(valueN){
 }
 
 
+//合一
+
+export function calculationFormula2(valueN){
+    if (valueN !== 0){
+        let arrayFormula = ["1"];
+        let sum1 = 0;
+        let sum2 = 0;
+        let resultSum = 0;
+        
+        for (var letterK = 2; letterK < (valueN+1); letterK++){
+            if (letterK % 2 == 0){
+                arrayFormula.push("+"+letterK);
+                }
+            else if (letterK % 2 !== 0){
+                arrayFormula.push("-"+letterK);
+                }
+            };
+        arrayFormula.push("=");
+
+        for (var letterI = 1; letterI < (valueN+1); letterI++){
+            if(letterI % 2 == 0){
+                sum1 = sum1+letterI;
+            }
+            else if(letterI % 2 != 0){
+                sum2 = sum2-letterI;
+            }
+        };
+        resultSum = sum1+sum2+2;
+        arrayFormula.push(resultSum);
+
+        return arrayFormula;
+    }
+    else{
+        let arrayValue0 = ["0=0"];
+        return arrayValue0;
+        };
+}
--- Q1_4/q1_4main.js
+++ Q1_4/q1_4main.js
@@ -1,7 +1,7 @@
 
 /*輸入 n 印出 `1+2-3+4-5+6...n` 的算式與總和 */
 
-import {calculationSUM, calculationFormula} from "./1_4module.js";
+import {calculationSUM, calculationFormula, calculationFormula2} from "./1_4module.js";
 import {isValidInteger} from "../common_modules/isInteger.js";
 
 import readline from'readline';
@@ -12,21 +12,38 @@ const read=readline.createInterface({
 
 read.question("輸入n值:",function (inputN) {
     if (isValidInteger(inputN)) {
-        const valueN = Number(inputN);
+        //const valueN = Number(inputN);
+        let valueN = Number(inputN);
         
-        //以下計算用
-        const sumFormula = calculationSUM(valueN);
-        //以下列出算式用
-        const arrayFormula = calculationFormula(valueN);
+        let arrayFormula = calculationFormula2(valueN)
         
-        console.log(arrayFormula.join("")+sumFormula);
+        console.log(arrayFormula.join(""));
         read.close();
     }
     else{
-        console.log("請輸入正確數字!");
+        console.log("請輸入正確數字");
         read.close();
     }
     
 });
 
 //用process.stdout.write做法因無回傳值,測試不好測;改成 列入陣列再用join輸出，有回傳值可以測試
+
+// read.question("輸入n值:",function (inputN) {
+//     if (isValidInteger(inputN)) {
+//         const valueN = Number(inputN);
+        
+//         //以下計算用
+//         const sumFormula = calculationSUM(valueN);
+//         //以下列出算式用
+//         const arrayFormula = calculationFormula(valueN);
+        
+//         console.log(arrayFormula.join("")+sumFormula);
+//         read.close();
+//     }
+//     else{
+//         console.log("請輸入正確數字!");
+//         read.close();
+//     }
+    
+// });
