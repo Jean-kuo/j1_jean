37a5a84ca9b12b700a0a3e63d98ca848011c357f
Jean Kuo <jeankuo5011@gmail.com>
2025-05-02 18:33:40 +0800

    加入另一種計算方式,用遞迴改寫中

--- Q1_4/1_4module.js
+++ Q1_4/1_4module.js
@@ -75,39 +75,32 @@ export function calculationFormula2(valueN){
         };
 }
 
+//另一種計算 (-1)** 幾次方
+export function calculationSunPower(valueN){
+    let sumPower = 0;
+    let resultSumPower;
+    for (var letterI = 0; letterI <= (valueN); letterI++){
+        sumPower += (letterI * ((-1) ** (letterI)));
+    };
+    resultSumPower = sumPower+2;
+    
+    return resultSumPower; 
+}
 
-// export function calculationFormula2(valueN){
-//     if (valueN !== 0){
-//         let arrayFormula = ["1"];
-//         let sum1 = 0;
-//         let sum2 = 0;
-//         let resultSum = 0;
-        
-//         for (var letterK = 2; letterK < (valueN+1); letterK++){
-//             if (letterK % 2 == 0){
-//                 arrayFormula.push("+"+letterK);
-//                 }
-//             else if (letterK % 2 !== 0){
-//                 arrayFormula.push("-"+letterK);
-//                 }
-//             };
-//         arrayFormula.push("=");
 
-//         for (var letterI = 1; letterI < (valueN+1); letterI++){
-//             if(letterI % 2 == 0){
-//                 sum1 = sum1+letterI;
-//             }
-//             else if(letterI % 2 != 0){
-//                 sum2 = sum2-letterI;
-//             }
-//         };
-//         resultSum = sum1+sum2+2;
-//         arrayFormula.push(resultSum);
+//遞迴函數
 
-//         return arrayFormula;
-//     }
-//     else{
-//         let arrayValue0 = ["0=0"];
-//         return arrayValue0;
-//         };
-// }
+export function  calculationFormula3(valueN){
+    
+    if ((valueN) === 0){
+        return 1;
+    }
+    else{
+        let sumPower = 0;
+        let resultSumPower;
+        sumPower += (valueN * ((-1) ** (valueN)));
+        return sumPower + 1 + calculationFormula3(valueN - 1)
+    }
+    
+    
+}
--- Q1_4/q1_4main.js
+++ Q1_4/q1_4main.js
@@ -1,7 +1,7 @@
 
 /*輸入 n 印出 `1+2-3+4-5+6...n` 的算式與總和 */
 
-import {calculationSUM, calculationFormula, calculationFormula2} from "./1_4module.js";
+import {calculationSunPower, calculationFormula3, calculationFormula2} from "./1_4module.js";
 import {isValidInteger} from "../common_modules/isInteger.js";
 
 import readline from'readline';
@@ -12,16 +12,21 @@ const read=readline.createInterface({
 
 read.question("輸入n值:",function (inputN) {
     if (isValidInteger(inputN)) {
-        //const valueN = Number(inputN);
         let valueN = Number(inputN);
         
         let arrayFormula = calculationFormula2(valueN)
         
         console.log(arrayFormula.join(""));
+
+        let sunPower = calculationSunPower(valueN);
+        let sunRecur = calculationFormula3(valueN);
+        console.log(sunPower);
+        console.log(sunRecur);
+
         read.close();
     }
     else{
-        console.log("請輸入正確數字");
+        console.log("請輸入0或正整數");
         read.close();
     }
     
@@ -29,21 +34,3 @@ read.question("輸入n值:",function (inputN) {
 
 //用process.stdout.write做法因無回傳值,測試不好測;改成 列入陣列再用join輸出，有回傳值可以測試
 
-// read.question("輸入n值:",function (inputN) {
-//     if (isValidInteger(inputN)) {
-//         const valueN = Number(inputN);
-        
-//         //以下計算用
-//         const sumFormula = calculationSUM(valueN);
-//         //以下列出算式用
-//         const arrayFormula = calculationFormula(valueN);
-        
-//         console.log(arrayFormula.join("")+sumFormula);
-//         read.close();
-//     }
-//     else{
-//         console.log("請輸入正確數字!");
-//         read.close();
-//     }
-    
-// });
