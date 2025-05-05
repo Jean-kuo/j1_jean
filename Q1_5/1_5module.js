c274eb27f18985d84dca7837be6ab3e014553c3d
Jean Kuo <jeankuo5011@gmail.com>
2025-05-05 18:24:15 +0800

    驗證重新整合,umleven修正

--- Q1_5/1_5module.js
+++ Q1_5/1_5module.js
@@ -1,15 +1,36 @@
+import {isValidInteger} from "../common_modules/isInteger.js";
+
 //判斷輸入n值為n>=4 且 n為偶數  
 export function isValueNaEVENnumber(valueN){
-    //條件運算值簡潔寫法
     const resultValueN = (valueN >= 4) && (valueN % 2 == 0) ? true : false;
     return resultValueN;
 }
 
-//遞迴函數
-export function umleven(valueNisOK){
-    if (valueNisOK<=2){
+//判斷輸入n值為n>=4
+export function isValueOver4(valueN){
+    let resultOver4 = (valueN >= 4) ? true : false;
+    return resultOver4;
+}
+//判斷輸入n值為偶數
+export function isNumberEven(valueN){
+    let resultEven = (valueN % 2 === 0) ? true : false;
+    return resultEven;
+}
+
+//合一
+export function checkValueN(valueN){
+    if ( ! isValidInteger(valueN)) return "輸入數值非整數";
+    if ( ! isValueOver4(valueN)) return "輸入數值小於4";
+    if ( ! isNumberEven(valueN)) return "輸入數值非偶數";
+    
+    return valueN; 
+}
+
+//遞迴函數 增設中止條件 避免負數造成無窮遞迴（報錯：maximum call stack size exceeded）
+export function umleven(valueN){
+    if (valueN <= 2 || valueN < 0){
         return 0;
     }
-    let formulaSUM = valueNisOK * (valueNisOK - 2);
-    return formulaSUM + umleven(valueNisOK - 2);
+    let umlevenSum = valueN * (valueN - 2);
+    return umlevenSum + umleven(valueN - 2);
 }
--- Q1_5/q1_5main.js
+++ Q1_5/q1_5main.js
@@ -3,7 +3,7 @@
 (n最小為 4, 只會出現偶數)*/
 
 import {isValidInteger} from "../common_modules/isInteger.js";
-import {isValueNaEVENnumber, umleven} from "./1_5module.js";
+import {isValueNaEVENnumber, checkValueN, umleven} from "./1_5module.js";
 
 import readline from 'readline';
 const read=readline.createInterface({
@@ -11,44 +11,71 @@ const read=readline.createInterface({
     output:process.stdout,
 });
 
-console.log("求算 (2*4 + 4*6 + 6*8...+(n-2)*n) 總和為多少?")
+console.log("求算 (2*4 + 4*6 + 6*8...+(n-2)*n) 總和為多少?");
 
-function getValueNAndCheck(){
-    return new Promise ((resolve, reject) => {
-
-        read.question("請輸入偶數且最小為4的n值:", function(valueN){
-            
-            if (isValidInteger(valueN)) {
-                if (isValueNaEVENnumber(valueN)){
-                    let valueNisOK = Number(valueN);
-                    resolve(valueNisOK);
-                }
-                else{
-                    const notOKvalueN = "輸入的n值要為偶數且最小為4";
-                    reject(notOKvalueN);
-                }
-            }
-            else{
-                console.log("輸入n值非有效整數");
-                read.close();
-            }
-            //read.close();
-        });
-    });
-};
-
-getValueNAndCheck()
-.then((valueNisOK) => {
-    let formulaSUM = umleven(valueNisOK);
-    console.log("總和為:"+formulaSUM);
-    
-})
-
-.catch((notOKvalueN) => {
-    console.log(notOKvalueN);
-    
-})
-
-.finally(() => {
+read.question("輸入n值：", function(n) {
+    let m = checkValueN(n);
+    let result = umleven(m);
+    console.log(result);
     read.close();
-})
+  })
+
+// //修改ing
+// async function getValueN() {
+//     await read.question("請輸入偶數且最小為4的數值:", function(){
+//         let resultN = checkValueN(resultN);
+//         return resultN;
+//     })
+// };
+// let n = getValueN();
+// console.log(n);
+
+// let valueN = getValueN();
+
+// let resultUmleven = umleven(valueN);
+// console.log(resultUmleven);
+
+
+
+
+
+
+// function getValueNAndCheck(){
+//     return new Promise ((resolve, reject) => {
+
+//         read.question("請輸入偶數且最小為4的數值:", function(valueN){
+            
+//             if (isValidInteger(valueN)) {
+//                 if (isValueNaEVENnumber(valueN)){
+//                     let valueNisOK = Number(valueN);
+//                     resolve(valueNisOK);
+//                 }
+//                 else{
+//                     const notOKvalueN = "輸入的n值要為偶數且最小為4";
+//                     reject(notOKvalueN);
+//                 }
+//             }
+//             else{
+//                 console.log("輸入n值非有效整數");
+//                 read.close();
+//             }
+//         });
+//     });
+// };
+
+
+// getValueNAndCheck()
+// .then((valueNisOK) => {
+//     let formulaSUM = umleven(valueNisOK);
+//     console.log("總和為:"+formulaSUM);
+    
+// })
+
+// .catch((notOKvalueN) => {
+//     console.log(notOKvalueN);
+    
+// })
+
+// .finally(() => {
+//     read.close();
+// })
