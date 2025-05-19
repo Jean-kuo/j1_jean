f12ba26bc1504f040c7961f2a0c4ddee6fbe56c8
Jean Kuo <jeankuo5011@gmail.com>
2025-05-19 16:38:53 +0800

    使用者輸入名字function修改

--- Q1_6/q1_6main.js
+++ Q1_6/q1_6main.js
@@ -6,42 +6,55 @@
 //問題1.輸出名字中單數位置的字母
 //問題2.陣列的單數?還是人類的單數?
 
-import {getSplitName} from './1_6module';
+import {getSplitName} from './1_6module.js';
 
+let name = [];
 
-
-const readline = require( 'readline' );
+import  readline from 'readline';
 const read = readline.createInterface({
     input : process.stdin,
     output : process.stdout,
 });
+
 
 var q1 = read;
 var q2 = read;
 var q3 = read;
 var q4 = read;
 
-q1.question("輸入第一個名字:", function (input1) {
-    name.push(input1);
-    q2.question("輸入第二個名字:", function (input2){
-        name.push(input2);
-        q3.question("輸入第三個名字:", function (input3) {
-            name.push(input3);
-            q4.question("輸入第四個名字:", function (input4) {
-                name.push(input4);
-                console.log("name陣列:"+name);
-                getSplitName(0);//第一個名字
-                getSplitName(2);//第三個名字
-                //splitName(name);
-                read.close();
-            });
-        });
-    });
+// q1.question("輸入第一個名字:", function (input1) {
+//     name.push(input1);
+//     q2.question("輸入第二個名字:", function (input2){
+//         name.push(input2);
+//         q3.question("輸入第三個名字:", function (input3) {
+//             name.push(input3);
+//             q4.question("輸入第四個名字:", function (input4) {
+//                 name.push(input4);
+//                 console.log("name陣列:"+name);
+//                 getSplitName(0);//第一個名字
+//                 getSplitName(2);//第三個名字
+//                 //splitName(name);
+//                 read.close();
+//             });
+//         });
+//     });
     
-});
+// });
+async function pushName(){
+    
+    let number = 1;
+    while (number >= 4){
+        await read.question(`輸入第${number}個名字 `, function(inputName){
+            name.push(inputName);
+            console.log(name);
+            return number+=1;
+        })
+    }
+    console.log(name);
+    return name;
+}
 
-
-
+pushName();
 
 
 
@@ -84,18 +97,3 @@ q1.question("輸入第一個名字:", function (input1) {
 //     //return shiftA, shiftB; //javascript不能回傳一個值以上,此處只會回傳最後的shiftB
 //     return [a, b];//可用陣列回傳兩個值以上
 
-
-
-
-
-// //另一個寫法 填入1~4可求出第1~4個名字單數
-// function getSplitName (a) {
-//     let splitX = name[a].split('')
-//     let arrayX = []
-//     for (var i = 0; i <= (splitX.length); i++){
-//         if (i % 2 == 0){
-//             arrayX.push(splitX[i]);
-//         };
-//     };
-//     console.log(`第 ${a+1} 個名字字母單數輸出: ${arrayX.join("")} `);
-// }
