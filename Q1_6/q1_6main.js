fa8871cc9280f0e130f1fe1728bfd3000e2c064e
Jean Kuo <jeankuo5011@gmail.com>
2025-05-20 17:35:23 +0800

    使用者輸入名字部份完成

--- Q1_6/q1_6main.js
+++ Q1_6/q1_6main.js
@@ -5,7 +5,7 @@
 
 //問題1.輸出名字中單數位置的字母
 //問題2.陣列的單數?還是人類的單數?
-
+import {isLetters} from "../common_modules/isLetters.js";
 import {getSplitName} from './1_6module.js';
 
 
@@ -19,35 +19,46 @@ let name = [];
 
 
 function userInput(query){
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         read.question(query, answer => {
-            resolve(answer)
+            //驗正是否英文字母
+            if (isLetters(answer)) {
+                resolve(answer)
+            }
+            else{
+                reject("請輸入英文名字")
+            }
         });
     });
-}
+};
 async function pushName(){
     let number = 1;
     while (number <= 4){
-        let inputName = await userInput(`輸入第${number}個名字: `);
-        name.push(inputName);
-        number+=1;
+        try{
+            let inputName = await userInput(`輸入第${number}個名字: `);
+            name.push(inputName);
+            number+=1;
+        }
+        catch(error){
+            console.log(error)
+        }
     };
     read.close();
     return name;
-}
-// //pushName()此為async非同步函式
-// pushName().then(name => {
-//     console.log(name);
-// });
+};
 
 //另一種async寫法
 (async () => {
-    const nameList = await pushName();
-    console.log(name);
+    const name = await pushName();
+    console.log(name)
+    
 })();
 
+getSplitName(1, name);
 
-
+getSplitName(3, name);
+//出現錯誤cannot read properties of undefined，
+// 推估是因為pushName是非同步函式，反而name等不到值傳入就被getSplitName抓走了
 
 
 
