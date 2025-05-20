ea0b35ae767ac318768de791317c27a0a8c2ab69
Jean Kuo <jeankuo5011@gmail.com>
2025-05-20 17:32:24 +0800

    小修改正則

--- common_modules/isLetters.js
+++ common_modules/isLetters.js
@@ -1,5 +1,5 @@
 export function isLetters (keyWord){
-    const regex = /$^[a-zA-Z\s]+$/;
+    const regex = /^[a-zA-Z\s]+$/;
     //console.log(regex.test(keyWord));
     return regex.test(keyWord);
 }
